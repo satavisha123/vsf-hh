025d6f632b50c44e9968351a24ec1588934cca6a
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-12-17 18:57:09 +0530

    Update AppHeader.vue

--- AppHeader.vue
+++ AppHeader.vue
@@ -181,9 +181,12 @@
               <path
                 fill="currentColor"
                 d="M571.31 193.94l-22.63-22.63c-6.25-6.24-16.38-6.25-22.63 0l-11.31 11.31-28.91-28.9c5.63-21.31.36-44.9-16.35-61.61l-45.25-45.25C392.99 15.62 352.05 0 311.1 0c-40.95 0-81.9 15.62-113.14 46.86l67.88 67.88v18.75c0 8.05 1.62 15.91 4.5 23.27L22.77 387.89C-6.88 415.57-7.68 462.32 21 491c14.03 14.03 32.37 21 50.69 21 19.15 0 38.27-7.62 52.42-22.77l230.92-247.34c7.56 3 15.58 4.52 23.61 4.52 5.61 0 11.23-.73 16.7-2.17l28.91 28.9-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.63c6.25 6.24 16.37 6.25 22.63 0l113.14-113.14c6.23-6.25 6.23-16.38-.02-22.63zM89.02 456.47c-9.86 10.56-25.36 9.31-34.08.59-10.21-10.22-8.44-25.66.59-34.08L300.3 194.46l17.24 17.24L89.02 456.47zm380.47-205.96l-59.74-59.73-26.68 7.05c-9.5 2.52-15.12-3.61-15.4-3.88l-49.14-49.14c-2.98-2.98-4.69-7.1-4.69-11.31V94.86l-40.4-40.4C285.37 50.21 298.06 48 311.1 48c29.92 0 58.04 11.65 79.2 32.8l45.25 45.25c5.84 5.84 4.45 13.26 3.89 15.4l-7.05 26.69 59.74 59.73-22.64 22.64z"
-              ></path></svg
-            ><!-- <i class="far fa-hammer"></i> Font Awesome fontawesome.com -->Products</a
-          >
+              ></path>
+            </svg>
+            <HeaderNavigation :isMobile="isMobile" />
+
+            <LocaleSelector class="smartphone-only" />
+          </a>
         </li>
 
         <li class="for-the-items">
@@ -577,6 +580,12 @@ svg:not(:root).svg-inline--fa {
   background: white;
   padding: 20px;
 }
+::v-deep .sf-header-navigation-item__item {
+  display: block;
+}
+::v-deep .sf-header__navigation {
+  margin-left: 0px;
+}
 .sf-icon {
   --icon-color: #e4002b;
   margin-right: 9px;
@@ -587,7 +596,12 @@ svg:not(:root).svg-inline--fa {
   align-items: flex-end;
   max-width: calc(1300px + 2em);
   margin: 0 auto;
-  
+  font-family: Agenda, sans-serif;
+  padding: 2px;
+}
+.for-the-link:hover {
+  text-decoration: none;
+  border-bottom: 2px solid #e4002b;
 }
 .for-the-items {
   margin: 0 1em;
