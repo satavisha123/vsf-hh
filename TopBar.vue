1b2da701510222fe54a2f9492a8f5a4f3ec538cf
satavisha123 <86342023+satavisha123@users.noreply.github.com>
2021-12-20 16:13:53 +0530

    Update TopBar.vue

--- TopBar.vue
+++ TopBar.vue
@@ -1,30 +1,42 @@
 <template>
-  <SfTopBar class="topbar">
-    <template #left>
-      <SfButton class="sf-button--text">{{ $t('Help & FAQs') }}</SfButton>
-    </template>
-    <template #center>
-      <p>{{ $t('Download') }}</p>
-      <SfButton class="topbar__button sf-button--text">{{ $t('Find out more') }}</SfButton>
-    </template>
-    <template #right>
-      <LocaleSelector />
-    </template>
-  </SfTopBar>
+  <div class="for-topbar">
+    <div>
+      <ul class="list">
+        <li class="item"><a>Home Furniture</a></li>
+        <li class="item"><a> Homes & Cottages</a></li>
+        <li class="item"><a>BeautiTone</a></li>
+      </ul>
+    </div>
+    <div>
+      <div class="middle"><b>FREE SHIPPING</b><a> a store eligible order</a></div>
+    </div>
+    <div class="language">
+      <a
+        href=" https://www.homehardware.ca/fr/"
+        data-mz-lang="languageChangeLink"
+        title="Français"
+        class="mz-utilitynav-link"
+        data-gtm-vis-recent-on-screen-8904058_1287="8072"
+        data-gtm-vis-first-on-screen-8904058_1287="8072"
+        data-gtm-vis-total-visible-time-8904058_1287="100"
+        data-gtm-vis-has-fired-8904058_1287="1"
+        >Français</a
+      >
+    </div>
+  </div>
 </template>
 
 <script>
-import { SfButton, SfTopBar } from '@storefront-ui/vue';
-import LocaleSelector from './LocaleSelector';
+import { SfButton, SfTopBar } from "@storefront-ui/vue";
+import LocaleSelector from "./LocaleSelector";
 
 export default {
   components: {
     SfTopBar,
     SfButton,
-    LocaleSelector
-  }
+    LocaleSelector,
+  },
 };
-
 </script>
 <style lang="scss" scoped>
 .topbar {
@@ -33,5 +45,40 @@ export default {
   &__button {
     margin: 0 0 0 var(--spacer-xs);
   }
+}
+.for-topbar {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.list {
+  list-style: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+.middle{
+  padding: 15px;
+}
+.middle:hover{
+   text-decoration: underline;
+}
+.item {
+  color: black;
+  font-size: 14px;
+  line-height: 14px;
+  border-left: 1px solid #000;
+  /* padding-right: 14px; */
+  padding-left: 14px;
+  float: left;
+  line-height: 20px;
+  text-decoration: underline;
+}
+
+.language{
+  padding-left: 300px;
+    padding-top: 15px;
+}
+.language:hover{
+   text-decoration: underline;
 }
 </style>
